71939752f4096b2bc66ac475ead0dcf194c33854
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-02-03 04:03:40 +0530

    Is this working?

--- app/main.js
+++ app/main.js
@@ -1,14 +1,18 @@
 const net = require('net');
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log('Logs from your program will appear here!');
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
+    socket.write(httpResponse);
+    socket.end();
+  });
+
   socket.on('close', () => {
     socket.end();
     server.close();
   });
 });
 
-server.listen(4221, 'localhost');
+server.listen(4221, 'localhost', () => {
+  console.log('Listening on 4221...');
+});
