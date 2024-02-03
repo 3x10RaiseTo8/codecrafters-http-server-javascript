6bc54d5097b6ad4c54cf5c2d165aa8cbbabde495
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-02-03 08:26:53 +0530

    Makeshift post try #1

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-const { existsSync, readFileSync } = require('fs');
+const { existsSync, readFileSync, writeFileSync } = require('fs');
 const net = require('net');
 const { resolve } = require('path');
 
@@ -12,6 +12,7 @@ const server = net.createServer((socket) => {
 
     const ok = 'HTTP/1.1 200 OK';
     const error = 'HTTP/1.1 404 Not found';
+    const created = 'HTTP/1.1 201 Created';
     const typeText = 'Content-Type: text/plain';
     const typeOctet = 'Content-Type: application/octet-stream';
     const enter = '\r\n';
@@ -19,15 +20,23 @@ const server = net.createServer((socket) => {
     if (path === '/') {
       socket.write(ok + enter + enter);
     } else if (path.match(/\/files/)) {
-      const file = path.slice(7);
-      const filePath = resolve(process.argv[3], file);
+      const filePath = resolve(process.argv[3], path.slice(7));
       console.log('filePath', filePath);
+
+      if (method === 'POST') {
+        const fileContent = data.toString.split('\r\n');
+        writeFileSync(filePath, fileContent);
+        socket.write(created + enter + enter);
+        socket.end();
+      }
+
       if (!existsSync(filePath)) {
         console.log("Path doesn't exist");
         socket.write(error + enter + enter);
         return socket.end();
       }
       console.log('Exists!');
+
       const fileStream = readFileSync(filePath);
       const response = [
         ok,
