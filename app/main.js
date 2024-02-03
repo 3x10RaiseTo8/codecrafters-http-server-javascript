230fb0c2fc578d84fba0fe294900ac24f9c65ec1
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-02-03 06:26:40 +0530

    Stage 5 aaaaaaaa

--- app/main.js
+++ app/main.js
@@ -1,33 +1,43 @@
 const net = require('net');
 
 const server = net.createServer((socket) => {
-  socket.on('data', (request) => {
-    const [method, path] = request.toString().split(' ');
+  socket.on('data', (data) => {
+    const request = data.toString().split(/\s+/);
+    const path = request[1];
 
     const ok = 'HTTP/1.1 200 OK';
     const error = 'HTTP/1.1 404 Not found';
+    const typeText = 'Content-Type: text/plain';
     const enter = '\r\n';
 
     if (path === '/') {
       socket.write(ok + enter + enter);
-      socket.end();
-    }
-    if (path.startsWith('/echo/')) {
-      const randomString = path.slice(6);
-      const type = 'Content-Type: text/plain';
+    } else if (path.match(/\/user-agent/)) {
+      const userAgent = request[request.indexOf('User-Agent:') + 1];
+      console.log(request);
+      console.log(userAgent);
       const response = [
         ok,
-        'Content-Type: text/plain',
+        typeText,
+        `Content-Length: ${userAgent.length}`,
+        '',
+        userAgent,
+        '',
+      ];
+      socket.write(response.join(enter));
+    } else if (path.startsWith('/echo/')) {
+      const randomString = path.slice(6);
+      const response = [
+        ok,
+        typeText,
         `Content-Length: ${randomString.length}`,
         '',
         randomString,
         '',
       ];
       socket.write(response.join(enter));
-      socket.end();
     } else {
       socket.write(error + enter + enter);
-      socket.end();
     }
   });
 
