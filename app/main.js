c17c0fe2c09874ea96edf62be49244bb45473139
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-02-03 05:30:45 +0530

    Works?

--- app/main.js
+++ app/main.js
@@ -1,24 +1,33 @@
 const net = require('net');
 
 const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
-    const [method, path] = data.toString().split(' ');
+  socket.on('data', (request) => {
+    const [method, path] = request.toString().split(' ');
 
-    const ok = 'HTTP/1.1 200 OK\r\n\r\n';
-    const error = 'HTTP/1.1 404 Not found\r\n\r\n';
+    const ok = 'HTTP/1.1 200 OK';
+    const error = 'HTTP/1.1 404 Not found';
+    const enter = '\r\n';
 
     if (path === '/') {
-      socket.write(ok);
+      socket.write(ok + enter + enter);
       socket.end();
     }
     if (path.startsWith('/echo/')) {
       const randomString = path.slice(6);
       const type = 'Content-Type: text/plain';
-      const length = `Content-Length: ${randomString.length}`;
-      const response = [ok, type, length, '', randomString, ''];
-      socket.write(response.join('\r\n'));
+      const response = [
+        ok,
+        'Content-Type: text/plain',
+        `Content-Length: ${randomString.length}`,
+        '',
+        randomString,
+        '',
+      ];
+      socket.write(response.join(enter));
+      socket.end();
     } else {
-      socket.write(error);
+      socket.write(error + enter + enter);
+      socket.end();
     }
   });
 
