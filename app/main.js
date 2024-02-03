a002d86063d209c7e1b662a5a83606620d055332
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-02-03 07:35:11 +0530

    Stage 7 man

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+const { existsSync, readFileSync } = require('fs');
 const net = require('net');
 
 // concurrency already implemented?
@@ -10,14 +11,29 @@ const server = net.createServer((socket) => {
     const ok = 'HTTP/1.1 200 OK';
     const error = 'HTTP/1.1 404 Not found';
     const typeText = 'Content-Type: text/plain';
+    const typeOctet = 'Content-Type: application/octet-stream';
     const enter = '\r\n';
 
     if (path === '/') {
       socket.write(ok + enter + enter);
+    } else if (path.match(/\/files/)) {
+      const file = path.slice(7);
+      const filePath = `${process.argv[3]}/${file}`;
+      if (!existsSync(filePath)) {
+        return socket.write(error);
+      }
+      const fileStream = readFileSync(filePath, 'utf-8');
+      const response = [
+        ok,
+        typeOctet,
+        `Content-Length: ${fileStream.length}`,
+        '',
+        fileStream,
+        '',
+      ];
+      socket.write(response.join(enter));
     } else if (path.match(/\/user-agent/)) {
       const userAgent = request[request.indexOf('User-Agent:') + 1];
-      console.log(request);
-      console.log(userAgent);
       const response = [
         ok,
         typeText,
